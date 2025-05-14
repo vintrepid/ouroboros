82b33c90250a284e267c275700ba6385fef2f786
Vince Nibler <vintrepid@pm.me>
2025-05-13 19:52:07 -0700

    around and around

--- mix.exs
+++ mix.exs
@@ -27,45 +27,73 @@ defmodule Ouroboros.MixProject do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
-      {:phoenix, ">= 0.0.0"}, #~> 1.7.11"},
-      {:phoenix_ecto, ">= 0.0.0"}, #~> 4.4"},
-      {:ecto_sql, ">= 0.0.0"}, #~> 3.10"},
-      {:postgrex, ">= 0.0.0"}, #>= 0.0.0"},
-      {:phoenix_live_view, ">= 0.0.0"}, #~> 0.20.2"},
-      {:phoenix_html, ">= 0.0.0"}, #~> 4.0"},
-      {:phoenix_live_reload, ">= 0.0.0"}, #~> 1.4", only: :dev},
-      {:phoenix_live_dashboard, ">= 0.0.0"}, #~> 0.8.3"},
-      {:telemetry_metrics, ">= 0.0.0"}, #~> 1.0"},
-      {:telemetry_poller, ">= 0.0.0"}, #~> 1.1.0"},
-      {:gettext, ">= 0.0.0"}, #~> 0.23"},
-      {:jason, ">= 0.0.0"}, #~> 1.4"},
-      {:plug, "~> 1.16", override: true},
-      {:plug_cowboy, ">= 0.0.0"}, #~> 2.7"},
-      {:plug_crypto, ">= 0.0.0"}, #~> 2.1.0"},
-      # {:phx_gen_auth, github: "phoenixframework/phx_gen_auth", only: [:dev], runtime: false},
-      {:bodyguard, ">= 0.0.0"}, #~> 2.4"},
-      {:livebook, github: "livebook-dev/livebook", tag: "v0.15.5", only: :dev},
-      {:fun_with_flags, ">= 0.0.0"}, #~> 1.8"},
-      # {:petal_admin, ">= 0.0.0"}, #~> 0.6"},
-      {:sentry, ">= 0.0.0"}, #~> 9.1"},
-      {:honeybadger, ">= 0.0.0"}, #~> 0.17"},
-      {:appsignal, ">= 0.0.0"}, #~> 2.7"},
-      # {:beacon, ">= 0.0.0"}, #~> 0.1.11"},
-      {:credo, ">= 0.0.0"}, #~> 1.7", only: [:dev, :test], runtime: false},
-      {:oban, ">= 0.0.0"}, #~> 2.17"},
-      {:swoosh, ">= 0.0.0"}, #~> 1.5"},
-      {:finch, ">= 0.0.0"}, #~> 0.16"},
-      {:flop, ">= 0.0.0"}, #~> 0.22"},
-      {:flop_phoenix, ">= 0.0.0"}, #~> 0.20"},
-      {:vega_lite, ">= 0.0.0"}, #~> 0.1"},
-      {:timex, ">= 0.0.0"}, #~> 3.7"},
-      {:esbuild, ">= 0.0.0"}, #~> 0.7", runtime: Mix.env() == :dev},
-      {:tailwind, ">= 0.0.0"}, #~> 0.2", runtime: Mix.env() == :dev},
-      {:excoveralls, ">= 0.0.0"}, #~> 0.18", only: :test}
+      # Phoenix and liveview
+      {:phoenix, "~> 1.7.11"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 4.0"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.20.2"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:heroicons,
+       github: "tailwindlabs/heroicons",
+       tag: "v2.1.1",
+       sparse: "optimized",
+       app: false,
+       compile: false,
+       depth: 1},
+
+      # Email
+      {:swoosh, "~> 1.5"},
+      {:finch, "~> 0.13"},
+
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.20"},
+      {:jason, "~> 1.2"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:bandit, "~> 1.2"},
+
+      {:plug_cowboy, "~> 2.7"},
+
+      # Dev & tooling
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+
+      # Authentication & authz
+      {:bodyguard, "~> 2.4"},
+
+      # Livebook (latest, GitHub tag)
+      # {:livebook, github: "livebook-dev/livebook", tag: "v0.15.5", only: :dev},
+
+      # A/B testing & feature flags
+      {:fun_with_flags, "~> 1.13"},
+
+      # Monitoring & error tracking
+      {:sentry, "~> 10.9"},
+      {:honeybadger, "~> 0.24"},
+      {:appsignal, "~> 2.15"},
+
+      # Background jobs
+      {:oban, "~> 2.19"},
+
+      # CMS, Admin, UI (commented for now)
+      # {:petal_admin, "~> 0.6"},
+      # {:beacon, "~> 0.1.11"},
+
+      # Data tools
+      {:flop, "~> 0.26"},
+      {:flop_phoenix, "~> 0.22"},
+      {:vega_lite, "~> 0.1"},
+      {:timex, "~> 3.7"}
     ]
   end
 
